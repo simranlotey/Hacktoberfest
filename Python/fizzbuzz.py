aec1fb737fe4f4ea8657cb7a7b91c0160ea8096b
Yumaris <57877322+cronousz@users.noreply.github.com>
2023-10-18 23:22:35 +0700

    Delete Python/fizzbuzz.py

--- Python/fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-for num in range(1, 50):
-    if num % 15 == 0:
-        print("FizzBuzz")
-    elif num % 3 == 0:
-        print("Fizz")
-    elif num % 5 == 0:
-        print("Buzz")
-    else:
-        print(num)
