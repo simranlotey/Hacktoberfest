86086b038b38abd65815a9545bb8ffc55bf2f20d
Cameron Hetzler <cjhetz@gmail.com>
2023-10-18 19:14:53 -0400

    Create a Java FizzBuzz script

---
+++ java/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,14 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println("FizzBuzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.printf("%d%n", i);
+        }
+    }
+}
