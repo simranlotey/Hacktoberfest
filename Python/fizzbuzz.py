cc74cbf0f8379b4773eff4268d62d82071432289
Yumaris <57877322+cronousz@users.noreply.github.com>
2023-10-18 23:05:25 +0700

    added fizzbuzz

---
+++ Python/fizzbuzz.py
@@ -0,0 +1,9 @@
+for num in range(1, 50):
+    if num % 15 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
