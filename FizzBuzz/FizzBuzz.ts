133c2bbd026b65bf4bc9b0f4b04562531a17eef7
unknown <dineshgarje102@gmail.com>
2023-10-19 13:26:26 +0530

    Add FizzBuzz.ts and FizzBuzz.cpp

---
+++ FizzBuzz/FizzBuzz.ts
@@ -0,0 +1,16 @@
+function FizzBuzz(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+let n: number = 30;
+FizzBuzz(n);
