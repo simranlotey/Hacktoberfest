3497e8ca092c8e03a735789179bbbcf80c13016d
Adithya <96947631+gh-adithya@users.noreply.github.com>
2023-10-19 12:21:41 +0530

    Add FizzBuzz.py

---
+++ FizzBuzz/FizzBuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+#Replace n with the range required
+fizzbuzz(100)
