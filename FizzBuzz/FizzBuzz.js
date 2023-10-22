a0ea8096001071262859db70c87d2ff0a0d021b3
ashajyothi06 <120666428+ashajyothi06@users.noreply.github.com>
2023-10-22 14:38:29 +0530

    committed changes

--- FizzBuzz/FizzBuzz.js
+++ FizzBuzz/FizzBuzz.js
@@ -1,5 +1,6 @@
 // FizzBuzz implementation in JavaScript
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++)
+ {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log('FizzBuzz');
     } else if (i % 3 === 0) {
